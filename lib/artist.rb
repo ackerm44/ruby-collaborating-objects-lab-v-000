97936028e54ba988c4e5e06ab546f84ac7558e1b
Amanda Ackerman <ackerm44@gmail.com>
2017-11-01 18:21:17 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -30,11 +30,11 @@ class Artist
     self.all.detect {|artist| artist.name == name}
   end
 
-  # #def self.create(name)
-  #   artist = Artist.new(name)
-  #   artist.save
-  #   artist
-  # end
+  def self.create(name)
+    artist = Artist.new(name)
+    artist.save
+    artist
+  end
 
   def self.find_or_create_by_name(name)
     if self.find(name).nil?
