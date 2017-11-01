2f0a224da4702342379df596807399c39a1ee9b2
Amanda Ackerman <ackerm44@gmail.com>
2017-11-01 18:30:09 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,9 +5,9 @@ class Song
     @name = name
   end
 
-  def artist_name=(name)
-    Artist.find_or_create_by_name(name)
-  end
+  # def artist_name=(name)
+  #   Artist.find_or_create_by_name(name)
+  # end
 
   def self.new_by_filename(filename)
     song_name_split = filename.split(" - ")[1]
