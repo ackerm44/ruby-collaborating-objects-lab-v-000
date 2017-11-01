71bbf267b056c39aadb2adbcf06d3256ec1010fc
Amanda Ackerman <ackerm44@gmail.com>
2017-11-01 19:08:51 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,7 +7,7 @@ class Song
 
   def artist_name=(name)
     @artist_name = name
-    Artist.find_or_create_by_name(@artist_name)
+    Artist.find_or_create_by_name(name)
   end
 
   def self.new_by_filename(filename)
