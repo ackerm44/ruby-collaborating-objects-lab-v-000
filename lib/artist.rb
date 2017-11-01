9b2d137ea92795eca9b4089f845e5d6ef9c322b8
Amanda Ackerman <ackerm44@gmail.com>
2017-11-01 18:04:03 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -26,22 +26,13 @@ class Artist
   end
 
   def self.find_or_create_by_name(name)
-    @@all.detect {|artist| artist.name == name}
-    if nil
-      self.new(name)
-    end
-    #else
-    #  self.new(name)
-    #end
-
-
-  #   @@all.detect do |artist|
-  #    if artist.name == name
-  #      self.name
-  #    else
-  #      self.new(name)
-  #    end
-  #  end
+    @@all.detect do |artist|
+     if artist.name == name
+       self.name
+     else
+       self.new(name)
+     end
+   end
  end
 
   def print_songs
