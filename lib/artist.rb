47538df8ad8e0520d5291d7a32fc99b7c9ec998d
Amanda Ackerman <ackerm44@gmail.com>
2017-11-01 18:16:21 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -24,10 +24,11 @@ class Artist
 
   def save
     @@all << self
+    puts @@all
   end
 
   def self.find(name)
-    self.all.detect {|artist| artist.name == name}
+    @@all.detect {|artist| artist.name == name}
   end
 
   def self.create(name)
