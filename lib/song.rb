cd6ecbb2de4d50c9d415ee7af9299185162b87e4
Amanda Ackerman <ackerm44@gmail.com>
2017-11-01 18:28:42 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,7 +13,8 @@ class Song
     song_name_split = filename.split(" - ")[1]
     artist_name_split = filename.split(" - ")[0]
     new_song = Song.new(song_name_split)
-    new_song.artist_name = Artist.name
+    new_song.artist_name = artist_name_split
+    new_song.artist = Artist
     new_song
 
   end
