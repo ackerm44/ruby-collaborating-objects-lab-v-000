ac47201dc56bd815660494c5fdde4a29b14a319f
Amanda Ackerman <ackerm44@gmail.com>
2017-11-01 19:22:11 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -39,7 +39,8 @@ class Artist
   def self.find_or_create_by_name(name)
     @@all.detect do |artist|
       if artist.name != name
-        self.new(name)
+        artist = self.new(name)
+        
       else
         name
       end
